bfc59dbe7ba49cab605b647e52b24c9a5bed6e4f
DudzinskiR <robert.dudzinski97@gmail.com>
2025-03-08 17:18:41 +0100

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,23 +10,25 @@ const Navbar = () => {
   return (
     <div
       className={twJoin(
-        "bg-background-secondary/90 w-full flex justify-center items-center fixed z-100 duration-300 px-10 backdrop-blur-sm",
+        "bg-background-secondary/90 w-full flex justify-center fixed z-100 duration-300 px-10 backdrop-blur-sm",
         isScrolled ? "h-[50px]" : "h-[70px]"
       )}
     >
-      <Link
-        className="page-content text-lg md:text-2xl font-anton min-w-24"
-        href={"/"}
-      >
-        FILM INFO
-      </Link>
-      <div className="flex flex-row gap-10 mr-16 md:text-base text-xs">
-        <Link href={"/movie"} className="hover:underline text-center">
-          EKSPLORUJ
+      <div className="page-content flex justify-between items-center h-full">
+        <Link
+          className="text-lg md:text-2xl font-anton min-w-24 hover:text-gray-300"
+          href={"/"}
+        >
+          FILM INFO
         </Link>
-        <Link href={"/search"} className="hover:underline">
-          SZUKAJ
-        </Link>
+        <div className="flex flex-row gap-10 mr-16 md:text-base text-xs">
+          <Link href={"/movie"} className="hover:underline text-center">
+            EKSPLORUJ
+          </Link>
+          <Link href={"/search"} className="hover:underline">
+            SZUKAJ
+          </Link>
+        </div>
       </div>
     </div>
   );
